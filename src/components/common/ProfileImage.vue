0a2a4663b1dced8cf5e26b6b54caafcb49ec3f29
Sabbirz <sabbir.kuasha@gmail.com>
2024-06-24 13:29:54 +0600

    update

---
+++ src/components/common/ProfileImage.vue
@@ -0,0 +1,11 @@
+<script setup>
+let imageLink = 'https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg'
+</script>
+
+<template>
+  <div class="avatar placeholder">
+    <div class="bg-neutral text-neutral-content rounded-full w-12">
+      <img v-bind:src="imageLink" alt="ProfileImage" />
+    </div>
+  </div>
+</template>
